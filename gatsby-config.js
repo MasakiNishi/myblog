4b812a41ccbfe019364c00f80ae4db49e77b27d4
MasakiNishi <n24mfs@gmail.com>
2021-05-02 20:45:15 +0900

    for live

--- gatsby-config.js
+++ gatsby-config.js
@@ -95,13 +95,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/posts/`,
-        name: "posts"
-      }
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
         path: `${__dirname}/content/pages/`,
         name: "pages"
       }
